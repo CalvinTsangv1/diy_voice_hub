1d756a86a2692a68bf77740654d8c931860ca5c2
Calvin Tsang <calvin.hs.tsang@prudential.com.hk>
2022-08-27 12:52:41 +0800

    Update pins.py

--- src/aiy/pins.py
+++ src/aiy/pins.py
@@ -93,7 +93,8 @@ from gpiozero.threads import GPIOThread
 
 def get_pin_offset():
     cmd = 'cat /sys/module/gpio_aiy_io/drivers/platform:gpio-aiy-io/gpio-aiy-io/gpio/gpiochip*/base'
-    return int(float(subprocess.run(cmd, shell=True, capture_output=True).stdout.strip()))
+    print (subprocess.run(cmd, shell=True, capture_output=True).stdout.strip())
+    return int(subprocess.run(cmd, shell=True, capture_output=True).stdout.strip())
 
 PIN_OFFSET = get_pin_offset()
 
