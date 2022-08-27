ced189564aeaacb6f0d807a1eabfc096ff11b8d3
Calvin Tsang <calvin.hs.tsang@prudential.com.hk>
2022-08-27 19:25:13 +0800

    Update audio.py

--- src/aiy/voice/audio.py
+++ src/aiy/voice/audio.py
@@ -261,9 +261,9 @@ def play_wav_async(filename_or_data):
 def play_mp3(filename_or_data):
     play_mp3_async(filename_or_data).wait()
 
-def play_mp3_async(fmt, filename_or_data):
+def play_mp3_async(filename_or_data):
     if isinstance(filename_or_data, str):
-        cmd = vlcPlayer(fmt=fmt, filetype='raw', filename=filename_or_data)
+        cmd = vlcPlayer(fmt=None, filetype='raw', filename=filename_or_data)
         return subprocess.Popen(cmd)    
 
 def play_mp3_pause():
