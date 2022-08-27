4c8b1400ea29a44fed2d1c07c1332ab086cea453
Calvin Tsang <calvin.hs.tsang@prudential.com.hk>
2022-08-27 12:46:12 +0800

    change int to float

--- src/examples/gpiozero/led_chaser.py
+++ src/examples/gpiozero/led_chaser.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append("/home/pi/diy_voice_hub/src")
 from time import sleep
 from gpiozero import LED
-from aiy.pins import (PIN_A, PIN_B, PIN_C, PIN_D)
+from aiy.pins import (float(PIN_A), float(PIN_B), float(PIN_C), float(PIN_D))
 
 leds = (LED(PIN_A), LED(PIN_B), LED(PIN_C), LED(PIN_D))
 while True:
