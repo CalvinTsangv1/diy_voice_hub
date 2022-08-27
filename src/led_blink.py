a7170898b0aed455813464dfdc756d0474f54d57
Calvin Tsang <calvin.hs.tsang@prudential.com.hk>
2022-08-27 14:24:58 +0800

    Create led_blink.py

---
+++ src/led_blink.py
@@ -0,0 +1,69 @@
+import sys
+import os
+ 
+# getting the name of the directory
+# where the this file is present.
+current = os.path.dirname(os.path.realpath(__file__))
+ 
+# Getting the parent directory name
+# where the current directory is present.
+parent = os.path.dirname(current)
+ 
+# adding the parent directory to
+# the sys.path.
+sys.path.append(parent)
+import argparse
+import locale
+import logging
+
+from aiy.board import Board, Led
+from aiy.cloudspeech import CloudSpeechClient
+
+
+def get_hints(language_code):
+    if language_code.startswith('en_'):
+        return ('turn on the light',
+                'turn off the light',
+                'blink the light',
+                'goodbye')
+    return None
+
+def locale_language():
+    language, _ = locale.getdefaultlocale()
+    return language
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser(description='Assistant service example.')
+    parser.add_argument('--language', default=locale_language())
+    args = parser.parse_args()
+
+    logging.info('Initializing for language %s...', args.language)
+    hints = get_hints(args.language)
+    client = CloudSpeechClient()
+    with Board() as board:
+        while True:
+            if hints:
+                logging.info('Say something, e.g. %s.' % ', '.join(hints))
+            else:
+                logging.info('Say something.')
+            text = client.recognize(language_code=args.language,
+                                    hint_phrases=hints)
+            if text is None:
+                logging.info('You said nothing.')
+                continue
+
+            logging.info('You said: "%s"' % text)
+            text = text.lower()
+            if 'turn on the light' in text:
+                board.led.state = Led.ON
+            elif 'turn off the light' in text:
+                board.led.state = Led.OFF
+            elif 'blink the light' in text:
+                board.led.state = Led.BLINK
+            elif 'goodbye' in text:
+                break
+
+if __name__ == '__main__':
+    main()
